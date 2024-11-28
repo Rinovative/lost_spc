d534fcab99f418933aff6e1577d5acef42849a28
Philip Trauth <trauthp@gmail.com>
2024-11-28 10:44:55 +0100

    Moved tests into separate functions (#25)
    * Moved tests into separate functions
    
    * Renamed doctests action to tests
    
    * pytest options into pyproject
    
    * sorted dependencies
    
    * Added pytest coverage
    
    * pytest cov to gitignore
    
    * Test coverage
    
    ---------
    
    Co-authored-by: Philip Trauth <philip.trauth@ost.ch>

--- tests/test_cards.py
+++ tests/test_cards.py
@@ -9,63 +9,61 @@ import lost_spc.constants as const
 import lost_spc.plots as plot
 import lost_spc.utils as ut
 
-df = pd.read_csv(str(Path("./tests/vane.txt")), sep=" ", header=None)
-print(df.head())
-array = df.to_numpy()
-sample_size = ut.get_sample_size(array)
-n = sample_size.n
-m = sample_size.m
-print("Zeilen x Spalten: ", n, "x", m)
 
-# %% a) mean, range, std
-ybar = val.calculate_means(array)
-R = val.calculate_ranges(array)
-std = val.calculate_standard_deviations(array)
-print(ybar)
-print(R)
-print(std)
+def test_cards():
+    """Sollte noch aufgeteilt werden in mehrere Tests für eine bessere Übersicht"""
+    df = pd.read_csv(str(Path("./tests/vane.txt")), sep=" ", header=None)
+    print(df.head())
+    array = df.to_numpy()
+    sample_size = ut.get_sample_size(array)
+    n = sample_size.n
+    m = sample_size.m
+    print("Zeilen x Spalten: ", n, "x", m)
 
-d = const.get_d(m)
-d2 = d.d2
-d3 = d.d3
-print(d2, d3)
+    # %% a) mean, range, std
+    ybar = val.calculate_means(array)
+    R = val.calculate_ranges(array)
+    std = val.calculate_standard_deviations(array)
+    print(ybar)
+    print(R)
+    print(std)
 
-# %% b) R-chart
-control_limits = limits.calculate_control_limits(array, chart_type="R")
-UCL = control_limits["UCL"]
-CL = control_limits["CL"]
-LCL = control_limits["LCL"]
+    d = const.get_d(m)
+    d2 = d.d2
+    d3 = d.d3
+    print(d2, d3)
 
-print("R-Karte UCL: ", UCL)
-print("R-Karte CL: ", CL)
-print("R-Karte LCL: ", LCL)
+    # %% b) R-chart
+    control_limits = limits.calculate_control_limits(array, chart_type="R")
+    UCL = control_limits["UCL"]
+    CL = control_limits["CL"]
+    LCL = control_limits["LCL"]
 
-# plots
-# Erstelle eine Figur mit Subplots
-fig, axes = plt.subplots(1, 2, figsize=(8, 10))
+    print("R-Karte UCL: ", UCL)
+    print("R-Karte CL: ", CL)
+    print("R-Karte LCL: ", LCL)
 
-# Erster Subplot: R-Chart
-plot.shewhart_card(UCL, CL, LCL, R, title="R-chart", ylabel=r"$R_i$", ax=axes[0])
+    # plots
+    # Erstelle eine Figur mit Subplots
+    _, axes = plt.subplots(1, 2, figsize=(8, 10))
 
-# Zweiter Subplot: R-Karte aus der Klasse
-rchart = plot.cards.R()
-rchart.fit(array)
-rchart.transform(array, ax=axes[1])
+    # Erster Subplot: R-Chart
+    plot.shewhart_card(UCL, CL, LCL, R, title="R-chart", ylabel=r"$R_i$", ax=axes[0])
 
-# Subplots optimieren und anzeigen
-plt.tight_layout()
-plt.show()
+    # Zweiter Subplot: R-Karte aus der Klasse
+    rchart = plot.cards.R()
+    rchart.fit(array)
+    rchart.transform(array, ax=axes[1])
 
-plot.plots.plot_histogram_normal(array.flatten())
-plt.show()
+    # Subplots optimieren und anzeigen
+    plt.tight_layout()
+    plot.plots.plot_histogram_normal(array.flatten())
+    plot.plots.plot_qq_plot(array.flatten())
 
-plot.plots.plot_qq_plot(array.flatten())
-plt.show()
+    print(limits.get_confidence_interval_cp(val.calculate_cp(UCL, LCL, std.mean()), array, 0.95))
 
-print(limits.get_confidence_interval_cp(val.calculate_cp(UCL, LCL, std.mean()), array, 0.95))
-
-array.reshape(-1)
-ewmachart = plot.cards.EWMA(lambda_=0.2, plot_calibration_data=True)
-ewmachart.fit(array)
-ewmachart.transform(array + 2)
-plt.show()
+    array.reshape(-1)
+    ewmachart = plot.cards.EWMA(lambda_=0.2, plot_calibration_data=True)
+    ewmachart.fit(array)
+    ewmachart.transform(array + 2)
+    # plt.show()
--- tests/test_power_oc_arl.py
+++ tests/test_power_oc_arl.py
@@ -3,88 +3,96 @@ import numpy as np
 
 from lost_spc.calculations.spc_values import ARL, ARL_R, oc, oc_r, power
 
-# power, OC und ARL x_bar-Karte
-k = np.linspace(-4, 4, 500)
-sigma = 0.0262
-mu = 1.2023 + k * sigma
 
-_, ax = plt.subplots(1, 3, figsize=(15, 5))
+def test_power_oc_arl():
+    """power, OC und ARL x_bar-Karte"""
+    plt.close("all")
+    k = np.linspace(-4, 4, 500)
+    sigma = 0.0262
+    mu = 1.2023 + k * sigma
 
-ax[0].plot(mu, power(k, 5), label="m = 5")
-ax[0].plot(mu, power(k, 10), label="m = 10")
-ax[0].plot(mu, power(k, 15), label="m = 15")
+    _, ax = plt.subplots(1, 3, figsize=(15, 5))
 
-ax[0].legend()
-ax[0].grid()
-ax[0].set_xlabel("Wahrer Erwartungswert des Prozessmerkmales")
-ax[0].set_title(r"Gütefunktion für die $\bar x$-Karte")
+    ax[0].plot(mu, power(k, 5), label="m = 5")
+    ax[0].plot(mu, power(k, 10), label="m = 10")
+    ax[0].plot(mu, power(k, 15), label="m = 15")
 
-k0 = np.linspace(0, 4)
-ax[1].plot(k0, oc(k0, 5), label="m = 5")
-ax[1].plot(k0, oc(k0, 10), label="m = 10")
-ax[1].plot(k0, oc(k0, 15), label="m = 15")
+    ax[0].legend()
+    ax[0].grid()
+    ax[0].set_xlabel("Wahrer Erwartungswert des Prozessmerkmales")
+    ax[0].set_title(r"Gütefunktion für die $\bar x$-Karte")
 
-ax[1].legend()
-ax[1].grid()
-ax[1].set_xlabel(r"Abweichung von $\mu_0$ in Vielfachen von $\sigma$")
-ax[1].set_title(r"Operationscharakteristik für die $\bar x$-Karte")
+    k0 = np.linspace(0, 4)
+    ax[1].plot(k0, oc(k0, 5), label="m = 5")
+    ax[1].plot(k0, oc(k0, 10), label="m = 10")
+    ax[1].plot(k0, oc(k0, 15), label="m = 15")
 
-ax[2].plot(mu, ARL(k=k, m=5))
-ax[2].grid()
-ax[2].set_xlabel("Wahrer Erwartungswert des Prozesses")
-ax[2].set_title(r"Durchschnittliche Lauflänge für die $\bar x$-Karte")
-plt.tight_layout()
-plt.show()
+    ax[1].legend()
+    ax[1].grid()
+    ax[1].set_xlabel(r"Abweichung von $\mu_0$ in Vielfachen von $\sigma$")
+    ax[1].set_title(r"Operationscharakteristik für die $\bar x$-Karte")
 
-# power, OC und ARL x_bar-Karte R-Karte
-_, ax = plt.subplots(1, 2, figsize=(15, 5))
-
-lam = np.linspace(1, 5, 500)
-ax[0].plot(lam, oc_r(lam=lam, m=5), label="m=5")
-ax[0].plot(lam, oc_r(lam=lam, m=10), label="m=10")
-ax[0].plot(lam, oc_r(lam=lam, m=15), label="m=15")
-
-ax[0].legend()
-ax[0].grid()
-ax[0].set_xlabel("Verhältnis der Prozessstreuung in Phase II bzgl. Phase I")
-ax[0].set_title(r"Operationscharakteristik für die $R$-Karte")
-
-ax[1].plot(lam, ARL_R(lam=lam, m=5), label="m=5")
-ax[1].plot(lam, ARL_R(lam=lam, m=10), label="m=10")
-ax[1].plot(lam, ARL_R(lam=lam, m=15), label="m=15")
-
-ax[1].legend()
-ax[1].grid()
-ax[1].set_xlabel("Verhältnis der Prozessstreuung in Phase II bzgl. Phase I")
-ax[1].set_title(r"Mittlere Lauflänge der $R$-Karte")
-plt.tight_layout()
-plt.show()
+    ax[2].plot(mu, ARL(k=k, m=5))
+    ax[2].grid()
+    ax[2].set_xlabel("Wahrer Erwartungswert des Prozesses")
+    ax[2].set_title(r"Durchschnittliche Lauflänge für die $\bar x$-Karte")
+    plt.tight_layout()
+    # plt.show()
 
 
-# Berechnung der ARL für verschiedene m-Werte und Darstellung
-_, ax = plt.subplots(1, 2, figsize=(15, 5))
+def test_power_oc_arl_r():
+    """power, OC und ARL x_bar-Karte R-Karte"""
+    plt.close("all")
+    _, ax = plt.subplots(1, 2, figsize=(15, 5))
 
-# X̄ control chart
-k = np.linspace(0, 2, 200)
+    lam = np.linspace(1, 5, 500)
+    ax[0].plot(lam, oc_r(lam=lam, m=5), label="m=5")
+    ax[0].plot(lam, oc_r(lam=lam, m=10), label="m=10")
+    ax[0].plot(lam, oc_r(lam=lam, m=15), label="m=15")
 
-for m in [2, 3, 5, 10]:
-    ax[0].plot(k, ARL(k=k, m=m), label=f"m={m}")
+    ax[0].legend()
+    ax[0].grid()
+    ax[0].set_xlabel("Verhältnis der Prozessstreuung in Phase II bzgl. Phase I")
+    ax[0].set_title(r"Operationscharakteristik für die $R$-Karte")
 
-ax[0].legend()
-ax[0].grid()
-ax[0].set_xlabel("$k = (\\mu_1 - \\mu_0) / \\sigma$")
-ax[0].set_title("ARL der $\\bar{X}$-Karte")
+    ax[1].plot(lam, ARL_R(lam=lam, m=5), label="m=5")
+    ax[1].plot(lam, ARL_R(lam=lam, m=10), label="m=10")
+    ax[1].plot(lam, ARL_R(lam=lam, m=15), label="m=15")
 
-# R-Karte
-lam = np.linspace(1, 2, 500)
+    ax[1].legend()
+    ax[1].grid()
+    ax[1].set_xlabel("Verhältnis der Prozessstreuung in Phase II bzgl. Phase I")
+    ax[1].set_title(r"Mittlere Lauflänge der $R$-Karte")
+    plt.tight_layout()
+    # plt.show()
 
-for m in [2, 3, 5, 10, 20]:
-    ax[1].plot(lam, ARL_R(lam=lam, m=m), label=f"m={m}")
 
-ax[1].legend()
-ax[1].grid()
-ax[1].set_xlabel("$\\lambda = \\sigma_1 / \\sigma_0$")
-ax[0].set_ylabel("ARL")
-ax[1].set_title("ARL der R-Karte")
-plt.tight_layout()
-plt.show()
+def test_arl_multiple_m():
+    """Berechnung der ARL für verschiedene m-Werte und Darstellung"""
+    plt.close("all")
+    _, ax = plt.subplots(1, 2, figsize=(15, 5))
+
+    # X̄ control chart
+    k = np.linspace(0, 2, 200)
+
+    for m in [2, 3, 5, 10]:
+        ax[0].plot(k, ARL(k=k, m=m), label=f"m={m}")
+
+    ax[0].legend()
+    ax[0].grid()
+    ax[0].set_xlabel("$k = (\\mu_1 - \\mu_0) / \\sigma$")
+    ax[0].set_title("ARL der $\\bar{X}$-Karte")
+
+    # R-Karte
+    lam = np.linspace(1, 2, 500)
+
+    for m in [2, 3, 5, 10, 20]:
+        ax[1].plot(lam, ARL_R(lam=lam, m=m), label=f"m={m}")
+
+    ax[1].legend()
+    ax[1].grid()
+    ax[1].set_xlabel("$\\lambda = \\sigma_1 / \\sigma_0$")
+    ax[0].set_ylabel("ARL")
+    ax[1].set_title("ARL der R-Karte")
+    plt.tight_layout()
+    # plt.show()
